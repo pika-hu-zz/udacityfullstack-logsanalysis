7a16644e4aaf766c18b350d34e6fe79301ad8a23
Raymond Hu <raymond29hu@gmail.com>
2019-05-11 19:48:14 -0400

    docs: generate-report.py skeleton documentation

---
+++ generate-report.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import psycopg2
+
+# Queries
+# Three most popular articles
+
+# Most popular article authors
+
+# Days in which more than 1% of requests lead to errors
+
+
+# Database query function
+# Opens connection, executes query, closes connection.
+
+
+# Print report
+
+
+# Main function
