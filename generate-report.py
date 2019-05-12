1555cf8040e08c8040a58a800848a919f5144ee6
Raymond Hu <raymond29hu@gmail.com>
2019-05-11 21:47:01 -0400

    feat: added query for and print pop. authors

--- generate-report.py
+++ generate-report.py
@@ -14,6 +14,14 @@ requestArticles = (
 )
 
 # Most popular article authors
+requestAuthors = (
+    "select authors.name, count(*) as views "
+    "from articles, authors, log "
+    "where authors.id = articles.author "
+    "and articles.slug = substr(log.path, 10) "
+    "group by authors.name "
+    "order by views desc;"
+)
 
 # Days in which more than 1% of requests lead to errors
 
@@ -29,13 +37,20 @@ def get_queryResults(query_request):
     return results
 
 # Print report
-def printArticles():
-    resultsAuthors = get_queryResults(requestArticles)
+def printReport():
+    resultsArticles = get_queryResults(requestArticles)
+    resultsAuthors = get_queryResults(requestAuthors)
+
     print("Top 3 most popular articles of all time:\n")
-    for title, views in resultsAuthors:
-        print(title," - ",views," views")
+    for title, views in resultsArticles:
+        print("{:<35}{:^3}{:>10} views".format(title, "-", views))
+    print("\n")
+
+    print("Most popular authors:\n")
+    for authors, views in resultsAuthors:
+        print("{:<35}{:^3}{:>10} views".format(authors, "-", views))
     print("\n")
 
 # Main function
 if __name__ == '__main__':
-    printArticles()
+    printReport()
